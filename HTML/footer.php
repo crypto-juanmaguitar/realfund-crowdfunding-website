b15e332fcefb6d1a1927b2c3aef97177907c577d
Rutrus <rutrus@gmail.com>
2019-05-27 19:02:05 +0200

    header & footer

--- HTML/footer.php
+++ HTML/footer.php
@@ -1,48 +1,48 @@
-    <footer id="footer">
+<footer id="footer">
         <div class="container_12 main-footer">
             <div class="grid_3 about-us">
-                <h3 class="rs title">About</h3>
-                <p class="rs description">Donec rutrum elit ac arcu bibendum rhoncus in vitae turpis. Quisque fermentum gravida eros non faucibus. Curabitur fermentum, arcu sed cursus commodo.</p>
+                <h3 class="rs title">Sobre nosotros</h3>
+                <p class="rs description">Real Fund es la empresa líder en tokenización de activos inmobiliarios</p>
                 <p class="rs email"><a class="fc-default  be-fc-orange" href="mailto:info@realfund.rocks">info@realfund.rocks</a></p>
-                <p class="rs">+1 (555) 555 - 55 - 55</p>
+                <p class="rs">+34 647734684</p>
             </div><!--end: .contact-info -->
             <div class="grid_3 recent-tweets">
-                <h3 class="rs title">Recent Tweets</h3>
+                <h3 class="rs title">Tweets Recientes</h3>
                 <div class="lst-tweets" id="sys_lst_tweets">
-                    
+
                 </div>
             </div><!--end: .recent-tweets -->
             <div class="clear clear-2col"></div>
             <div class="grid_3 email-newsletter">
-                <h3 class="rs title">Newsletter Signup</h3>
+                <h3 class="rs title">Suscríbete a nuestra Newsletter</h3>
                 <div class="inner">
-                    <p class="rs description">Nam aliquet, velit quis consequat interdum, odio dolor elementum.</p>
+                    <p class="rs description">Te tendremos al día con las novedades en el mercado</p>
                     <form action="#">
                         <div class="form form-email">
                             <label class="lbl" for="txt-email">
                                 <input id="txt-email" type="text" class="txt fill-width" placeholder="Enter your e-mail address"/>
                             </label>
-                            <button class="btn btn-green" type="submit">Submit</button>
+                            <button class="btn btn-green" type="submit">Enviar</button>
                         </div>
                     </form>
                 </div>
             </div><!--end: .email-newsletter -->
             <div class="grid_3">
-                <h3 class="rs title">Discover &amp; Create</h3>
+                <h3 class="rs title">Descubre &amp; Crea</h3>
                 <div class="footer-menu">
                     <ul class="rs">
-                        <li><a class="be-fc-orange" href="#">What is Real Fund</a></li>
-                        <li><a class="be-fc-orange" href="#">Start a project</a></li>
-                        <li><a class="be-fc-orange" href="#">Project Guidlines</a></li>
-                        <li><a class="be-fc-orange" href="#">Press</a></li>
-                        <li><a class="be-fc-orange" href="#">Stats</a></li>
+                        <li><a class="be-fc-orange" href="#">Qué es Real Fund</a></li>
+                        <li><a class="be-fc-orange" href="#">¿Tienes un proyecto?</a></li>
+                        <li><a class="be-fc-orange" href="#">Líneas maestras del proyecto</a></li>
+                        <li><a class="be-fc-orange" href="#">Prensa</a></li>
+                        <li><a class="be-fc-orange" href="#">Estadísticas</a></li>
                     </ul>
                     <ul class="rs">
                         <li><a class="be-fc-orange" href="#">Staff Picks</a></li>
                         <li><a class="be-fc-orange" href="#">Popular</a></li>
-                        <li><a class="be-fc-orange" href="#">Recent</a></li>
-                        <li><a class="be-fc-orange" href="#">Small Projects</a></li>
-                        <li><a class="be-fc-orange" href="#">Most Funded</a></li>
+                        <li><a class="be-fc-orange" href="#">Reciente</a></li>
+                        <li><a class="be-fc-orange" href="#">Nuestras joyitas</a></li>
+                        <li><a class="be-fc-orange" href="#">Los más financiados</a></li>
                     </ul>
                     <div class="clear"></div>
                 </div>
@@ -54,13 +54,13 @@
                 <div class="grid_12">
                     <a class="logo-footer" href="index.html"><img src="images/logo-2.png" alt="Real Fund"/></a>
                     <p class="rs term-privacy">
-                        <a class="fw-b be-fc-orange" href="single.php">Terms & Conditions</a>
+                        <a class="fw-b be-fc-orange" href="single.html">Términos y Condiciones</a>
                         <span class="sep">/</span>
-                        <a class="fw-b be-fc-orange" href="single.php">Privacy Policy</a>
+                        <a class="fw-b be-fc-orange" href="single.html">Política de privacidad</a>
                         <span class="sep">/</span>
                         <a class="fw-b be-fc-orange" href="#">FAQ</a>
                     </p>
-                    <p class="rs ta-c fc-gray-dark site-copyright">© <a href="http://realfund.rocks" title="Real Fund" target="_blank">RealFund</a> 2019.</p>
+                    <p class="rs ta-c fc-gray-dark site-copyright"><a href="http://realfund.rocks" title="Real Fund" target="_blank">Real Fund © 2019</a>.</p>
                 </div>
                 <div class="clear"></div>
             </div>
