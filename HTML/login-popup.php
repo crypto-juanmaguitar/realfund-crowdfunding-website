33f2ecfc46ba9b25e25388a4503789cee05bdec0
Rutrus <rutrus@gmail.com>
2019-05-28 18:03:56 +0200

    separate login popup

---
+++ HTML/login-popup.php
@@ -0,0 +1,77 @@
+<div class="popup-common" id="sys_popup_common">
+    <div class="overlay-bl-bg"></div>
+    <div class="container_12 pop-content">
+        <div class="grid_12 wrap-btn-close ta-r">
+            <i class="icon iBigX closePopup"></i>
+        </div>
+        <div class="grid_6 prefix_1">
+            <div class="form login-form">
+                <form action="#">
+                    <h3 class="rs title-form">Registro</h3>
+                    <div class="box-white">
+                        <h4 class="rs title-box">Nuevo por aquí?</h4>
+                        <p class="rs">Se requiere una cuenta de Real Fund para continuar.</p>
+                        <div class="form-action">
+                            <label for="txt_name">
+                                <input id="txt_name" class="txt fill-width" type="text" placeholder="Nombre completo"/>
+                            </label>
+                            <div class="wrap-2col clearfix">
+                                <div class="col">
+                                    <label for="txt_email">
+                                        <input id="txt_email" class="txt fill-width" type="email" placeholder="Email"/>
+                                    </label>
+                                    <label for="id_card">
+                                        <input id="id_card" class="txt fill-width" type="text" placeholder="DNI"/>
+                                    </label>
+                                </div>
+                                <div class="col">
+                                    <label for="txt_password">
+                                        <input id="txt_password" class="txt fill-width" type="password" placeholder="Contraseña"/>
+                                    </label>
+                                    <label for="txt_re_password">
+                                        <input id="txt_re_password" class="txt fill-width" type="password" placeholder="Repite la contraseña"/>
+                                    </label>
+                                </div>
+                            </div>
+                            <p class="rs pb10">Registrándote aceptas nuestros <a href="#" class="fc-orange">condiciones de uso</a> y nuestra <a href="#" class="fc-orange">política de privacidad</a>.</p>
+                            <p class="rs ta-c">
+                                <button class="btn btn-red btn-submit" type="submit">Registrarse</button>
+                            </p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="grid_4">
+            <div class="form login-form">
+                <form action="#">
+                    <h3 class="rs title-form">Login</h3>
+                    <div class="box-white">
+                        <h4 class="rs title-box">Tienes ya una cuenta?</h4>
+                        <p class="rs">Haz login para continuar.</p>
+                        <div class="form-action">
+                            <label for="txt_email_login">
+                                <input id="txt_email_login" class="txt fill-width" type="email" placeholder="Tu email"/>
+                            </label>
+                            <label for="txt_password_login">
+                                <input id="txt_password_login" class="txt fill-width" type="password" placeholder="Contraseña"/>
+                            </label>
+
+                            <label for="chk_remember" class="rs pb20 clearfix">
+                                <input id="chk_remember" type="checkbox" class="chk-remember"/>
+                                <span class="lbl-remember">Mantén abierta la sesión</span>
+                            </label>
+                            <p class="rs ta-c pb10">
+                                <button class="btn btn-red btn-submit" type="submit">Login</button>
+                            </p>
+                            <p class="rs ta-c">
+                                <a href="#" class="fc-orange">Olvidé mi contraseña</a>
+                            </p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
