55e22e1ffa118fddcd6214730a0f1af20d26513d
Rutrus <rutrus@gmail.com>
2019-05-28 17:45:16 +0200

    minor change

--- HTML/all-pages.php
+++ HTML/all-pages.php
@@ -122,18 +122,6 @@
         <div class="clear"></div>
     </div>
 
-    <div class="additional-info-line">
-        <div class="container_12">
-            <div class="grid_9">
-                <h2 class="rs title">Vestibulum tristique, purus eget euismod vulputate, nisl erat suscipit mi!</h2>
-                <p class="rs description">Duis placerat malesuada sapien, eu consequat mauris vestibulum vitae. Aliquam fermentum lorem ut leo ultricies semper. In sed ligula massa, vitae elementum mauris.</p>
-            </div>
-            <div class="grid_3 ta-r">
-                <a class="btn bigger btn-red" href="#">Learn more</a>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div><!--end: .additional-info-line -->
     <?php 
     include_once("additional-info.php");
     include_once("footer.php"); 
