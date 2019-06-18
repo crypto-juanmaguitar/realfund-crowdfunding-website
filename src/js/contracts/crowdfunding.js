4ba65d24f4be3efaf97f1abcc3d90a8132663671
JuanMa Garrido <juanma.garrido@gmail.com>
2019-06-18 12:54:10 +0300

    latest version somart contracts

--- src/js/contracts/crowdfunding.js
+++ src/js/contracts/crowdfunding.js
@@ -3,8 +3,8 @@ import web3 from './web3';
 import {abi} from 'realfund-smart-contracts/build/contracts/Crowdfunding.json'
 
 
-// const address = '0xA51eec858367065b1C3B41839362050E5f409540'
-const address = '0x2c20b238eBf33b09E55541f1D22421a8824c33aa'
+const address = '0xA51eec858367065b1C3B41839362050E5f409540'
+// const address = '0x2c20b238eBf33b09E55541f1D22421a8824c33aa'
 
 const instance = new web3.eth.Contract(abi, address);
 
